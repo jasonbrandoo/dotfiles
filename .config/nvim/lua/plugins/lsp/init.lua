cf9758fc15ba28850202437ce903be0d8ec951eb
Jason Brando <jasonbrandoo@gmail.com>
2026-03-19 14:22:41 +0700

    update

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -1,49 +1,52 @@
 return {
 	{
-		'neovim/nvim-lspconfig',
+		"neovim/nvim-lspconfig",
 		dependencies = {
 			{
-				'mason-org/mason.nvim',
+				"mason-org/mason.nvim",
 				opts = {},
 			},
-			'mason-org/mason-lspconfig.nvim',
-			'WhoIsSethDaniel/mason-tool-installer.nvim',
-			{ 'j-hui/fidget.nvim', opts = {} },
+			"mason-org/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			{ "j-hui/fidget.nvim", opts = {} },
 		},
 		config = function()
-			vim.api.nvim_create_autocmd('LspAttach', {
-				group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
 					local map = function(keys, func, desc, mode)
-						mode = mode or 'n'
-						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
-					map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
-					map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
-					map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+					map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+					map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client:supports_method('textDocument/documentHighlight', event.buf) then
-						local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-						vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+					if client and client:supports_method("textDocument/documentHighlight", event.buf) then
+						local highlight_augroup =
+							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 							buffer = event.buf,
 							group = highlight_augroup,
 							callback = vim.lsp.buf.document_highlight,
 						})
-						vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 							buffer = event.buf,
 							group = highlight_augroup,
 							callback = vim.lsp.buf.clear_references,
 						})
-						vim.api.nvim_create_autocmd('LspDetach', {
-							group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
 							callback = function(event2)
 								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
 							end,
 						})
 					end
-					if client and client:supports_method('textDocument/inlayHint', event.buf) then
-						map('<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, '[T]oggle Inlay [H]ints')
+					if client and client:supports_method("textDocument/inlayHint", event.buf) then
+						map("<leader>th", function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+						end, "[T]oggle Inlay [H]ints")
 					end
 				end,
 			})
@@ -55,18 +58,23 @@ return {
 					on_init = function(client)
 						if client.workspace_folders then
 							local path = client.workspace_folders[1].name
-							if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
+							if
+								path ~= vim.fn.stdpath("config")
+								and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+							then
+								return
+							end
 						end
-						client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+						client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
 							runtime = {
-								version = 'LuaJIT',
-								path = { 'lua/?.lua', 'lua/?/init.lua' },
+								version = "LuaJIT",
+								path = { "lua/?.lua", "lua/?/init.lua" },
 							},
 							workspace = {
 								checkThirdParty = false,
-								library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
-									'${3rd}/luv/library',
-									'${3rd}/busted/library',
+								library = vim.tbl_extend("force", vim.api.nvim_get_runtime_file("", true), {
+									"${3rd}/luv/library",
+									"${3rd}/busted/library",
 								}),
 							},
 						})
@@ -78,7 +86,7 @@ return {
 			}
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {})
-			require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 			for name, server in pairs(servers) do
 				vim.lsp.config(name, server)
 				vim.lsp.enable(name)
@@ -86,15 +94,17 @@ return {
 		end,
 	},
 	{
-		'saghen/blink.cmp',
-		version = '1.*',
+		"saghen/blink.cmp",
+		version = "1.*",
 		dependencies = {
 			{
-				'L3MON4D3/LuaSnip',
-				version = '2.*',
+				"L3MON4D3/LuaSnip",
+				version = "2.*",
 				build = (function()
-					if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then return end
-					return 'make install_jsregexp'
+					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+						return
+					end
+					return "make install_jsregexp"
 				end)(),
 				dependencies = {},
 				opts = {},
@@ -102,19 +112,19 @@ return {
 		},
 		opts = {
 			keymap = {
-				preset = 'default',
+				preset = "default",
 			},
 			appearance = {
-				nerd_font_variant = 'mono',
+				nerd_font_variant = "mono",
 			},
 			completion = {
 				documentation = { auto_show = false, auto_show_delay_ms = 500 },
 			},
 			sources = {
-				default = { 'lsp', 'path', 'snippets' },
+				default = { "lsp", "path", "snippets" },
 			},
-			snippets = { preset = 'luasnip' },
-			fuzzy = { implementation = 'lua' },
+			snippets = { preset = "luasnip" },
+			fuzzy = { implementation = "lua" },
 			signature = { enabled = true },
 		},
 	},
